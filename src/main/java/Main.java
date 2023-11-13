c8bd3bada6fb436c8c652e24c48ce0cdd0fd0e52
mark4802 <maje0011@stud.kea.dk>
2023-11-13 09:50:38 +0100

    testing eat in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
+
         dog.makeSound();
         cat.makeSound();
+
+        dog.eat("pancake");
+        cat.eat("fish");
     }
 }
